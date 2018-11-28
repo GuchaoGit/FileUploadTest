3214ec64ffa35c4bb9a4df28eeec29d3b031f24e
guchaochao <guchaochao@huiyunit.com>
2018-11-28 17:08:08 +0800

    下载工具测试

--- app/src/main/java/com/guc/fileuploadtest/aty/ActivityDownload.java
+++ app/src/main/java/com/guc/fileuploadtest/aty/ActivityDownload.java
@@ -4,19 +4,27 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 import com.guc.fileuploadtest.R;
 import com.zero.cdownload.CDownload;
 import com.zero.cdownload.listener.CDownloadListener;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 /**
  * Created by guc on 2018/8/31.
- * 描述：
+ * 描述：下载工具
  */
 public class ActivityDownload extends Activity {
+    @BindView(R.id.pb_progressbar)
+    ProgressBar mPbProgressbar;
+    @BindView(R.id.btn_dowmload)
+    Button mBtnDowmload;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,13 +42,28 @@ public class ActivityDownload extends Activity {
             }
 
             @Override
-            public void onProgress(long maxSIze, long currentSize) {
+            public void onProgress(final long maxSIze, final long currentSize) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        int cur = (int) (currentSize * 100 / maxSIze);
+                        mPbProgressbar.setProgress(cur);
+                        Log.e("guc", "in onProgress cur percent:" + cur);
+                        mBtnDowmload.setText(String.format("已下载：%d/100", cur));
+                    }
+                });
                 Log.e("guc", "in onProgress maxSIze:" + maxSIze + ";currentSize:" + currentSize);
             }
 
             @Override
-            public void onComplete(String localFilePath) {
+            public void onComplete(final String localFilePath) {
                 Log.e("guc", "onComplete localFilePath:" + localFilePath);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mBtnDowmload.setText(String.format("已下载：%s", localFilePath));
+                    }
+                });
             }
 
             @Override
